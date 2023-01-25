dbf26f2119391890bda088492e48aba2c14c8f15
tarikopenclassrooms <maarijtarik@hotmail.com>
2023-01-25 16:22:53 +0100

    Login page

---
+++ src/pages/Signin.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../store/actions/userActions";
+import  {useNavigate} from "react-router-dom"
+
+const Signin = () => {
+ const status = useSelector(state => state.user.status)
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (status == 'success'){
+      navigate('/user')
+    }
+  }, [status])
+  
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData)
+    dispatch(login(formData));
+  };
+
+  return (
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-wrapper">
+            <label htmlFor="email">Username</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="input-remember">
+            <input type="checkbox" id="remember-me" />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+
+          <button className="sign-in-button">Sign In</button>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default Signin;
