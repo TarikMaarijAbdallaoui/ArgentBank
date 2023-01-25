c4597ac7cfcfd5f760332283e7c28692afecefb6
tarikopenclassrooms <maarijtarik@hotmail.com>
2023-01-25 16:27:44 +0100

    Styles and app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import Features from "./components/Features/Features";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
-import Main from "./Main/Main";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./layout/layout";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import User from "./pages/User";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-
-      <Main>
-        <Hero />
-        <Features />
-      </Main>
-
-      <Footer />
-    </div>
+    <Routes>
+      <Route element={<Layout/>} >
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/user" element={<User />} />
+      </Route>
+    </Routes>
   );
 }
 
