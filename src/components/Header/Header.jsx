a318e3abf0ff7fd4629adc698cef437fb9cea2b3
tarikopenclassrooms <maarijtarik@hotmail.com>
2023-01-25 16:24:00 +0100

    Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import Login from '../Login/Login'
-import Logo from '../Logo/Logo'
-import '../../main.css'
+import React, { useState } from "react";
+import { LoggedIn, LoggedOut } from "../Login/Login";
+import Logo from "../Logo/Logo";
+import "../../main.css";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  
+  const status = useSelector(state => state.user.status)
   return (
     <nav className="main-nav">
-        <Logo />
-        <Login />
+      <Logo />
+      {status == "success" ? <LoggedIn /> : <LoggedOut />}
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import  logout from '../../store/slices/userSlice'
 import '../../main.css'
 
-const Login = () => {
+export const LoggedOut = () => {
   return (
         <div>
-        <a className="main-nav-item" href="./sign-in.html">
+        <Link className="main-nav-item" to="/signin">
           <i className="fa fa-user-circle"></i>
           Sign In
-        </a>
+        </Link>
       </div>
   )
 }
 
-export default Login
+export const LoggedIn = () => {
+  const  {firstName} = useSelector(state => state.user.info)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+        <Link className="main-nav-item" to="/user">
+          <i className="fa fa-user-circle"></i>
+          {firstName}
+        </Link>
+        <Link className="main-nav-item" to="/" onClick={() => dispatch(logout)}>
+          <i className="fa fa-sign-out"></i>
+          Sign Out
+        </Link>
+      </div>
+  )
+}
