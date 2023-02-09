86883e9ae36fc38c7d6595ea41b4b69ccf021c19
tarikopenclassrooms <maarijtarik@hotmail.com>
2023-02-09 19:19:40 +0100

    not found 404 page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="main">
+      <div>
+        <h1>404: Not Found</h1>
+        <p>The page you are looking for is not available.</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
